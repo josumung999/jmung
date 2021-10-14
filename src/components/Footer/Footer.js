31646f02421bda342276b04407cdbe4663335962
Josué Munganga <jmunganga039@gmail.com>
2021-10-14 12:03:57 +0200

    Finished setting other components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call:</LinkTitle>
+          <LinkItem href="tel:+25769888643">+257 69 888 643</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email:</LinkTitle>
+          <LinkItem href="mailto:jmunganga039@gmail.com">jmunganga039@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Bringing Industrious Technos to all of us</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/josumung999">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://instagram.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
