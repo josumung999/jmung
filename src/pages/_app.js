cf0408afa6ca4536444e0de7b42cb98335673184
Josué Munganga <jmunganga039@gmail.com>
2021-10-18 20:37:39 +0200

    Making the website SEO Friendly

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 import Theme from '../styles/theme';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="description" content="Joshua Munganga's Portfolio - JavaScript Software Engineer, Junior Web Developer, MERN Stack Developer" />
+        <title>Joshua Munganga - JavaScript Software Engineer</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
