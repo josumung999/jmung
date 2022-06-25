45dd2768c7b2b9022430f74c8d1e90eed4d97467
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-25 20:07:35 +0200

    MVP of simple Portfolio

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { LeftSection } from './HeroStyles';
+import { AiOutlineFilePdf } from 'react-icons/ai';
 
 const Hero = () => (
   <Section row nopadding>
@@ -14,9 +15,11 @@ const Hero = () => (
       <SectionText>
         Full Stack Software Engineer, Skilled in JavaScript and PHP <br />
         I work with the MERN stack, Laravel, Next.js, Serverless Architecture, MySQL, PostgreSQL <br />
-        <small>
-          <Link href="./resume/Joshua-Munganga.pdf" style={{ color: "white", marginTop: "16px", textDecoration: "none" }}>My Resume</Link>
-        </small>
+        <Link href="./resume/Joshua-Munganga.pdf">
+          <small  style={{ color: "#cecece", marginTop: "32px", textDecoration: "underline" }}>
+          <AiOutlineFilePdf /> My Resume
+          </small>
+        </Link>
       </SectionText>
     </LeftSection>
   </Section>
