331e20eb04944135e190bfbf937ecec96a5593ee
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-25 19:27:14 +0200

    Basic setup of Maintenance Page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+import { Container, Div1, Div3, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <div>
@@ -15,23 +15,6 @@ const Header = () =>  (
           </a>
         </Link>
       </Div1>
-      <Div2>
-        <li>
-          <Link href='#projects'>
-            <NavLink>Projects</NavLink>
-          </Link>
-        </li>
-        <li>
-          <Link href='#tech'>
-            <NavLink>Technologies</NavLink>
-          </Link>
-        </li>
-        <li>
-          <Link href='#about'>
-            <NavLink>About</NavLink>
-          </Link>
-        </li>
-      </Div2>
       <Div3>
         <SocialIcons href="https://github.com/josumung999" target="_blank">
           <AiFillGithub size="3rem" />
