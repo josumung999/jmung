124e0df5e769a3e1bb892b04c047ee5c04ff8370
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-25 19:50:05 +0200

    Built the sample resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,22 +2,22 @@ import React from 'react';
 import Link from 'next/link';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Hello There ! <br />
-        I'm Joshua
+        Hi ! <br />
+        I'm Joshua Munganga
       </SectionTitle>
       <SectionText>
-        Full Stack Software Engineer, aiming to build computer based solutions to real life problems
+        Full Stack Software Engineer, Skilled in JavaScript and PHP <br />
+        I work with the MERN stack, Laravel, Next.js, Serverless Architecture, MySQL, PostgreSQL <br />
+        <small>
+          <Link href="./resume/Joshua-Munganga.pdf" style={{ color: "white", marginTop: "16px", textDecoration: "none" }}>My Resume</Link>
+        </small>
       </SectionText>
-      <Link href='./resume/Joshua-Resume.pdf' target='blank' rel="noopener noreferrer">
-        <Button>My Resume</Button>
-      </Link>
     </LeftSection>
   </Section>
 );
